1db6a8f360563647c2d5f5c9803ccaea915efa33
neverett8fr <neverett8fr@btinternet.com>
2023-03-03 04:34:52 +0000

    add login form design

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div>
     <notifications></notifications>
-    <router-view :key="$route.fullPath"></router-view>
+    <LoginForm v-if="store.token == ''"></LoginForm>
+    <router-view v-else :key="$route.fullPath"></router-view>
   </div>
 </template>
 
 <script>
 
+import LoginForm from './pages/Login/LoginForm.vue';
 import { store } from './store/store';
 
 export default {
   data() {
     return {
       store: store,
-    }
+    };
   },
   methods: {
     toggleNavOpen() {
-      let root = document.getElementsByTagName('html')[0];
-      root.classList.toggle('nav-open');
+      let root = document.getElementsByTagName("html")[0];
+      root.classList.toggle("nav-open");
     }
   },
   mounted() {
-    this.$watch('$sidebar.showSidebar', this.toggleNavOpen)
-  }
+    this.$watch("$sidebar.showSidebar", this.toggleNavOpen);
+  },
+  components: { LoginForm }
 };
 </script>
 
---
+++ src/pages/Login/LoginForm.vue
@@ -0,0 +1,41 @@
+<!-- eslint-disable vue/no-mutating-props -->
+<template>
+    <card class="col-md-8">
+        <h5 slot="header" class="title">Edit Profile</h5>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="logo-img" onclick="alert('coming soon!')" src="img/anime6.png" alt="...">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 px-md-12">
+                <base-input label="Username" placeholder="Enter Username" v-model="model.username">
+                </base-input>
+            </div>
+            <div class="col-md-8 pl-md-1">
+                <base-input label="Password" type="Password" placeholder="Enter Password">
+                </base-input>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <base-button slot="footer" type="primary" fill>Login</base-button>
+                <base-button slot="footer" fill>Create Account</base-button>
+            </div>
+        </div>
+    </card>
+</template>
+<script>
+export default {
+    props: {
+        model: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        }
+    }
+};
+</script>
+
+  
